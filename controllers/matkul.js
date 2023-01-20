7bfb141b99c83a8056ad26e0dbf90b7a2a1c40a2
alfian <alfianwilfredohoris@gmail.com>
2023-01-21 00:44:23 +0700

    done with crud matkul

--- controllers/matkul.js
+++ controllers/matkul.js
@@ -8,5 +8,37 @@ class ControllerMatkul {
       console.log(error);
     }
   }
+  static async createMatkul(req, res) {
+    try {
+      let { nama } = req.body;
+      let createdMatkul = await Matkul.create({ nama });
+      res.status(201).json({
+        message: `Matkul ${nama} berhasil dimasukkan kedalam database dan mendapatkan id ${createdMatkul.id}`,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async updateMatkulName(req, res) {
+    try {
+      let { nama } = req.body;
+      let { id } = req.params;
+      let updatedMatkul = await Matkul.update({ nama }, { where: { id } });
+      res.json({ message: "Nama matkul berhasil di update" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async deleteMatkul(req, res) {
+    try {
+      let { id } = req.params;
+      let deletedMatkul = await Matkul.destroy({ where: { id } });
+      res.json({ message: "Matkul berhasil di delete" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 module.exports = ControllerMatkul;
--- routes/matkul.js
+++ routes/matkul.js
@@ -3,5 +3,8 @@ const matkul = require("../controllers/matkul");
 const app = express();
 
 app.get("/", matkul.getAll);
+app.post("/", matkul.createMatkul);
+app.patch("/:id", matkul.updateMatkulName);
+app.delete("/:id", matkul.deleteMatkul);
 
 module.exports = app;
