9fc61c0292bd0f9048ef34f82c872e59600edaab
alfian <alfianwilfredohoris@gmail.com>
2023-01-21 01:09:35 +0700

    done with all crud rencana studi

--- controllers/rencanaStudi.js
+++ controllers/rencanaStudi.js
@@ -9,5 +9,45 @@ class ControllerStudi {
       console.log(error);
     }
   }
+  static async createRencanaStudi(req, res) {
+    try {
+      let IdMahasiswa = +req.body.IdMahasiswa;
+      let IdMatkul = +req.body.IdMatkul;
+
+      let createdRencanaStudi = await RencanaStudi.create({
+        IdMahasiswa,
+        IdMatkul,
+      });
+      res
+        .status(201)
+        .json({ message: `Rencana studi berhasil ditambahkan ke database` });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async updateRencanaMatkul(req, res) {
+    try {
+      let { id } = req.params;
+      let IdMatkul = +req.body.IdMatkul;
+      let updatedRencanaMatkul = await RencanaStudi.update(
+        { IdMatkul },
+        { where: { id } }
+      );
+      res.json({ message: "Rencana Studi berhasil diubah" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async deleteRencanaStudi(req, res) {
+    try {
+      let { id } = req.params;
+      let deletedRencanaStudi = await RencanaStudi.destroy({ where: { id } });
+      res.json({ message: "Rencana studi berhasil dihapus" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 module.exports = ControllerStudi;
--- routes/studi.js
+++ routes/studi.js
@@ -3,5 +3,8 @@ const studi = require("../controllers/rencanaStudi");
 const app = express();
 
 app.get("/", studi.getAll);
+app.post("/", studi.createRencanaStudi);
+app.patch("/:id", studi.updateRencanaMatkul);
+app.delete("/:id", studi.deleteRencanaStudi);
 
 module.exports = app;
