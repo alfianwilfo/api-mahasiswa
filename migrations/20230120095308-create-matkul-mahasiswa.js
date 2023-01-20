3950391161cf403c145b98d5519f53ea54a9c5a1
alfian <alfianwilfredohoris@gmail.com>
2023-01-20 17:23:02 +0700

    init model

---
+++ migrations/20230120095308-create-matkul-mahasiswa.js
@@ -0,0 +1,45 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("MatkulMahasiswas", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      matkul: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Matkul",
+          key: "id",
+        },
+        onUpdate: "cascade",
+        onDelete: "cascade",
+      },
+      mahasiswa: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Mahasiswa",
+          key: "id",
+        },
+        onUpdate: "cascade",
+        onDelete: "cascade",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("MatkulMahasiswas");
+  },
+};
