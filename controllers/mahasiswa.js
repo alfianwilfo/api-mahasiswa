4f4fe3e6123fe0018b23b19169cb354bde6dcff4
alfian <alfianwilfredohoris@gmail.com>
2023-01-21 10:10:16 +0700

    create mahasiswa with validator

--- controllers/mahasiswa.js
+++ controllers/mahasiswa.js
@@ -12,26 +12,60 @@ class ControllerMahasiswa {
 
   static async findById(req, res) {
     try {
-      let id = req.params.id;
-
+      let id = +req.params.id;
       let data = await Mahasiswa.findByPk(id);
+      let input = { data: data };
+      let rules = { data: "required" };
+
+      let validation = new Validator(input, rules, {
+        required: "Mahasiswa tidak ditemukan",
+      });
+      function passes() {
+        res.json(data);
+      }
+      function fails() {
+        let msg = validation.errors.first("data");
+        throw { msg };
+      }
+      validation.checkAsync(passes, fails);
       res.json(data);
     } catch (error) {
-      console.log(error, "??");
+      if (error.msg) {
+        res.status(400).json({ message: error.msg });
+      } else {
+        res.status(500).json({ message: "Internal server error" });
+      }
     }
   }
 
   static async createMahasiswa(req, res) {
     try {
       let { nama } = req.body;
-      let validation = new Validator({ nama: nama }, { id: "required" });
-
-      let createdMahasiswa = await Mahasiswa.create({ nama });
-      res.status(201).json({
-        message: `${nama} berhasil ditambahkan kedalam database dan mendapatkan id ${createdMahasiswa.id}`,
-      });
+      let validator = new Validator(
+        {
+          nama,
+        },
+        {
+          nama: "required|min:3|regex:/^[a-zA-Z]*$/",
+        }
+      );
+      function fails() {
+        let msg = validator.errors.first("nama");
+        throw { msg };
+      }
+      async function passes() {
+        let createdMahasiswa = await Mahasiswa.create({ nama });
+        res.status(201).json({
+          message: `${nama} berhasil ditambahkan kedalam database dan mendapatkan id ${createdMahasiswa.id}`,
+        });
+      }
+      validator.checkAsync(passes, fails);
     } catch (error) {
-      console.log(error);
+      if (error.msg) {
+        res.status(400).json({ message: error.msg });
+      } else {
+        res.status(500).json({ message: "Internal server error" });
+      }
     }
   }
 
