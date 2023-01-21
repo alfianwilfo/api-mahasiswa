2c262512384da1b3b6e758b1ddfa9bba0fa7b0c0
alfian <alfianwilfredohoris@gmail.com>
2023-01-21 11:02:53 +0700

    done update mahasiswa with validator

--- controllers/mahasiswa.js
+++ controllers/mahasiswa.js
@@ -111,13 +111,80 @@ class ControllerMahasiswa {
     try {
       let { id } = req.params;
       let { nama } = req.body;
-      let updatedMahasiswa = await Mahasiswa.update(
-        { nama },
-        { where: { id } }
+
+      // res.json({ message: "Nama mahasiswa berhasil di update" });
+      let findMahasiswa = await Mahasiswa.findByPk(id);
+
+      let val = new Validator(
+        {
+          findMahasiswa,
+        },
+        {
+          findMahasiswa: "required",
+        },
+        {
+          required: "Mahasiswa not found",
+        }
       );
-      res.json({ message: "Nama mahasiswa berhasil di update" });
+      val.checkAsync(passFind, failsFind);
+
+      function passFind() {
+        let validator = new Validator(
+          {
+            nama,
+          },
+          {
+            nama: "required|regex:/^[a-zA-Z]*$/|min:3",
+          },
+          {
+            required: "You forgot to give a :attribute",
+            regex: ":attribute format invalid",
+            min: ":attribute length minimum 3 character",
+          }
+        );
+        function fails() {
+          let msg = validator.errors.first("nama");
+          throw { msg };
+        }
+        async function passes() {
+          let updatedMahasiswa = await Mahasiswa.update(
+            { nama },
+            { where: { id } }
+          );
+
+          let validateUpdate = new Validator(
+            {
+              updatedMahasiswa: updatedMahasiswa[0],
+            },
+            {
+              updatedMahasiswa: "accepted",
+            },
+            { accepted: "Fail to update nama mahasiswa" }
+          );
+          validateUpdate.checkAsync(passesUpdate, failsUpdate);
+          function passesUpdate() {
+            // Validation passed
+            res.json({ message: "Success update nama mahasiswa" });
+          }
+
+          function failsUpdate() {
+            let msg = validator.errors.first("updatedMahasiswa");
+            throw { msg };
+          }
+        }
+        validator.checkAsync(passes, fails);
+      }
+
+      function failsFind(params) {
+        let msg = val.errors.first("findMahasiswa");
+        throw { msg };
+      }
     } catch (error) {
-      console.log(error);
+      if (error.msg) {
+        res.status(404).json({ message: error.msg });
+      } else {
+        res.status(500).json({ message: "Internal server error" });
+      }
     }
   }
 }
